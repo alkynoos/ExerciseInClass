9a6c699800449853d7922782389ede70f3ddb09d
Alkynoos Moros <alkynoosmoros@gmail.com>
2020-11-11 15:00:10 +0200

    Add project files.

---
+++ ExerciseInClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseInClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Home home = new Home();
+            home.Name = "My home";
+            home.Address = "GoodFaith 8";
+            PrintHome print;
+            print = (name, address) => $"{name} {address}";
+            Console.WriteLine(print(home.Name,home.Address));
+
+        }
+        public delegate string PrintHome(string name, string address);
+    }
+}
